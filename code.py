90104b0e51b200f363ba488e1309be7c57bef326
H4RRY-B4WLS <95259732+H4RRY-B4WLS@users.noreply.github.com>
2021-11-29 19:52:48 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+#H4RRY-B4WLS
+
+import random
+import requests
+
+def getRndInt():
+    for _ in range(999999):
+        value = random.randint(100000, 999999)
+        response = requests.get(
+            f"https://api.blooket.com/api/firebase/id?id={value}")
+        data = response.json()
+
+        if data["success"] == True:
+            print('Valid Game Pin: ' + str(value))
+            quit("Pin Found!")
+            
+getRndInt();
