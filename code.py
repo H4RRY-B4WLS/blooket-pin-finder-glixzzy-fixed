4754b274d474c3647791757659ddf8adbf3004dc
H4RRY-B4WLS <95259732+H4RRY-B4WLS@users.noreply.github.com>
2022-06-26 19:44:36 -0400

    code.py

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ def getRndInt():
     for _ in range(999999):
         value = random.randint(100000, 999999)
         response = requests.get(
-            f"https://api.blooket.com/api/firebase/id?id={value}")
+            f"https://fb.blooket.com/c/firebase/id?id={value}")
         data = response.json()
 
         if data["success"] == True:
